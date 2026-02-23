d49ea4221a9facf278009ff8e803b8256bf5bb31
pengtao199 <pengtao2048@gmail.com>
2026-02-23 13:19:03 +0800

    Initial Commit

---
+++ cardlistdemo/cardlistdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardlistdemoApp.swift
+//  cardlistdemo
+//
+//  Created by mac on 2026/2/23.
+//
+
+import SwiftUI
+
+@main
+struct cardlistdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
